5d93637e75d0abb3d4d398aa5ed116a455087683
Maxwell Poll <179867674+MaxPollCS@users.noreply.github.com>
2025-09-14 22:05:38 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// Maxwell Poll
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int in_num1, in_num2, sum;
+
+    cout << "\nMy name is Max Poll" << endl;
+
+    cout << "\nEnter the first number > ";
+    cin >> in_num1;
+
+    cout << "\nEnter the second number > ";
+    cin >> in_num2;
+
+    sum = in_num1 + in_num2;
+    cout << "\nThe sum of " << in_num1 << " and " << in_num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+    
+    My name is Max Poll
+
+    Enter the first number > 33
+
+    Enter the second number > 34
+
+    The sum of 33 and 34 is 67
+
+    C:\Users\Maxwell\source\repos\lab0\x64\Debug\lab0.exe (process 41228) exited with code 0 (0x0).
+    To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+    Press any key to close this window . . .
+*/
